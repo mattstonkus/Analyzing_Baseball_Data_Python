adfe9ea00650a31f4ac8795324e199b6ae23756c
Matt Stonkus <matt.stonkus@gmail.com>
2021-03-13 17:04:00 -0800

    first two questions

--- get_data.py
+++ get_data.py
@@ -1,8 +1,26 @@
 import dload
 import os.path
 from os import path
+import pandas as pd
 
-if path.exists('master'):
-    print('Folder already exists')
-else:
-    dload.save_unzip("https://github.com/chadwickbureau/baseballdatabank/archive/master.zip")
+
+def get_data():
+    """
+    Function to download lahman data set, unzip and create a folder
+    """
+    if path.exists('master'):
+        print('Folder already exists')
+    else:
+        dload.save_unzip("https://github.com/chadwickbureau/baseballdatabank/archive/master.zip")
+
+
+def make_df(data_set: str) -> pd.DataFrame:
+    """
+    Simple function that saves writting out the file path
+    :param data_set: Name of Lahman dataset you want
+    :return: A dataframe of the dataset
+    """
+    assert isinstance(data_set, str)
+    df: pd.DataFrame = pd.read_csv(f'master/baseballdatabank-master/core/{data_set}.csv')
+    assert isinstance(df, pd.DataFrame)
+    return df
