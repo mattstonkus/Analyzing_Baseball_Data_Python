f9350638565ed89b950ac32912edf716c2be55c5
Matt Stonkus <matt.stonkus@gmail.com>
2021-03-22 19:45:51 -0700

    add in get_data for retro

--- get_data.py
+++ get_data.py
@@ -1,6 +1,6 @@
 import dload
 import os.path
-from os import path
+import os
 import pandas as pd
 
 
@@ -8,10 +8,10 @@ def get_data():
     """
     Function to download lahman data set, unzip and create a folder
     """
-    if path.exists('master'):
+    if os.path.exists('master'):
         print('Folder already exists')
     else:
-        dload.save_unzip("https://github.com/chadwickbureau/baseballdatabank/archive/master.zip")
+        dload.save_unzip("https://github.com/chadwickbureau/baseballdatabank/archive/master.zip", delete_after=True)
 
 
 def make_df(data_set: str) -> pd.DataFrame:
@@ -24,3 +24,9 @@ def make_df(data_set: str) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_csv(f'master/baseballdatabank-master/core/{data_set}.csv')
     assert isinstance(df, pd.DataFrame)
     return df
+
+def get_retro_data():
+    retro_path = './retro'
+    os.mkdir(retro_path)
+    for year in range(1871,2020):
+        dload.save_unzip(f'https://www.retrosheet.org/gamelogs/gl{year}.zip', extract_path=retro_path , delete_after=True)
